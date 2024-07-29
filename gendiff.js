3a1e9c965675e83545982fd9e2205b9823c61261
DariaKaduchka <Daria.Kaduchka@gmail.com>
2024-07-29 18:14:55 +0200

    added argumats and options to -h

--- gendiff.js
+++ gendiff.js
@@ -3,10 +3,12 @@ const { program } = require('commander');
 
 program
   .description('Compares two configuration files and shows a difference.Compares two configuration files and shows a difference.')
-  .version('1.0.0');
+  .version('1.0.0')
+  .argument('<filepath1><filepath2>')
+  .option('-f, --format [type]', 'output format')
 
 program.parse();
 
 const options = program.opts();
 
-export default genDiff; //ещё не написана
+// export default genDiff; //ещё не написана
